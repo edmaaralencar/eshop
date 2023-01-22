7100df55bea817027c2a3551f4e1770eda76d190
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-01-22 16:48:46 -0300

    fix: fix axios url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: `${
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://eshop-plum.vercel.app/'
+      : 'https://eshop-plum.vercel.app'
   }/api`
 })
